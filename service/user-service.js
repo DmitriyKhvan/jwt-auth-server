592d7fd227db09ce3a356ef640fd91662b217931
freedom_twin <dimon.twin@gmail.com>
2026-04-23 17:06:53 +0500

    first commit

---
+++ service/user-service.js
@@ -0,0 +1,88 @@
+const UserModel = require("../models/user-model");
+const RoleModel = require("../models/role-model");
+const tokenService = require("./token-service");
+const UserDto = require("../dtos/user-dto");
+const bcrypt = require("bcrypt");
+const ApiError = require("../exceptions/api-error");
+
+class UserService {
+  async registration(email, password) {
+    const candidate = await UserModel.findOne({ email });
+    if (candidate) {
+      throw ApiError.BadRequest(
+        `Пользователь с почтовым адресом ${email} уже существует`
+      );
+    }
+    const hashPassword = await bcrypt.hash(password, 3);
+    const userRole = await RoleModel.findOne({ value: "USER" });
+    const user = await UserModel.create({
+      email,
+      password: hashPassword,
+      roles: [userRole.value],
+    });
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async login(email, password) {
+    const user = await UserModel.findOne({ email });
+    if (!user) {
+      throw ApiError.BadRequest("Неверный email или пароль");
+    }
+
+    const isPassEquals = await bcrypt.compare(password, user.password);
+
+    if (!isPassEquals) {
+      throw ApiError.BadRequest("Неверный email или пароль");
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async logout(refreshToken) {
+    const token = await tokenService.removeToken(refreshToken);
+    return token;
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const userData = tokenService.validateRefreshToken(refreshToken);
+    const tokenFromDb = await tokenService.findToken(refreshToken);
+
+    console.log(tokenFromDb);
+
+    if (!userData || !tokenFromDb) {
+      throw ApiError.UnauthorizedError();
+    }
+
+    const user = await UserModel.findById(userData.id);
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateTokens({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { ...tokens, user: userDto };
+  }
+
+  async getAllUsers() {
+    const users = await UserModel.find();
+    return users;
+  }
+
+  async getUser(id) {
+    const user = await UserModel.findById(id);
+    return user;
+  }
+}
+
+module.exports = new UserService();
